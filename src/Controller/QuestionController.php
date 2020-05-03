099fc56ea700a536b0fdef1513dcb64cc1b4b5af
QAROUACH Azzeddine SCO IDF <GDGK5226@orange.com>
2020-05-03 16:54:12 +0200

    maj_030520_15h54

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -18,13 +18,21 @@ class QuestionController extends AbstractController
   }
 
     /**
-     * @Route("questions/{on_est_en_plein_Ramadan}")
+     * @Route("questions/{mon_prenom_est_Azzeddine}")
      */
-  public  function show($on_est_en_plein_Ramadan)
+  public  function show($mon_prenom_est_Azzeddine)
   {
+      $answers = [
+          'Mon premier enfants il s\'appel Marwane né le 01 Mai 2003',
+          'Mon  deuxième enfants  elle s\'appel Imane une fille  née le 19 Décembre 2004',
+          'Mon dernier enfants elle s\'appel Ines  une fille née le née le12 Novembre 2007'
+      ];
 
-   return new Response(sprintf(
+          return $this->render('question/show.html.twig',[
+          'question'=>str_ireplace('_',' ',$mon_prenom_est_Azzeddine),
+              'answers'=>$answers
+          ]);
 
-       'Bonjour : %s ', str_ireplace('_',' ',$on_est_en_plein_Ramadan)));
+
   }
 }
